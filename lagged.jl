1bb9dabe2c2dcb6bed9801c82f6c3367e9d0e164
Mauricio <mauriciogtec@gmail.com>
2020-03-24 13:54:17 -0500

    updates to lag model

--- lagged.jl
+++ lagged.jl
@@ -41,9 +41,10 @@ mutable struct LagModel
         @assert size(X, 1) == size(Y, 1)
         nobs, nrows, ncols = size(Y)
         _, nlags, nplants = size(X)
-        μ = zeros(nplants, nrows, ncols)
-        λ = zeros(nplants, nrows, ncols)
-        γ = zeros(nplants, nrows, ncols)
+        μ = zeros(Float32, (nplants, nrows, ncols))
+        λ = ones(Float32, (nplants, nrows, ncols))
+        γ = ones(Float32, (nplants, nrows, ncols))
+        α = 0.0f0
         new(μ, γ, λ, α,
             η, ν,
             X, Y, 
@@ -75,24 +76,26 @@ function kernel(
         μ::Float32,
         λ::Float32;
         derivatives::Bool=true)
-    # function
+    # eval kernel
+    C = √(2π)
     δ = @. μ - t
     Δ = @. 0.5 * δ ^ 2
     ψ = @. exp(- λ * Δ)
-    C = √(2π)
     β = @. λ * ψ / C
-    if !derivatives
+
+    if !derivatives 
         return β
     end
+
     # derivatives
     ∂μ = @. - λ * δ * β
+    ∂²μ = @. - λ * (β + δ * ∂μ)
     ∂λ = @. ψ / C - Δ * β
+    ∂²λ = @. - Δ * (ψ / C - ∂λ)
+
     ∇β = [∂λ, ∂μ]
-    # second derivatives
-    ∂²μ = @. - λ * β - λ * δ * ∂μ
-    ∂²λ = @. - Δ * ψ / C - Δ * ∂λ
     ∇²β = [∂²λ, ∂²μ]
-    # ∂μ∂λ = - width .* (β + λ .* ∂λ)
+
     return β, ∇β, ∇²β
 end
 
@@ -103,9 +106,9 @@ Each step is linear time with small constant.
 """
 function learn!(mod::LagModel)
     tvec = Float32.(0:(mod.T - 1))
-    starting_loss = 0.0
+    prev_loss = 0.0f0
     grid = [(r, c) for c in 1:mod.C for r in 1:mod.R]
-    new_α = 0.0f0   # we'll accumulate error to set new intercept
+    cum_error = 0.0f0   # we'll accumulate error to set new intercept
     for (r, c) in ProgressBar(grid)
         # params for row, col
         γ = view(mod.γ, :, r, c)
@@ -113,49 +116,54 @@ function learn!(mod::LagModel)
         λ = view(mod.λ, :, r, c)
         y = view(mod.Y, :, r, c)
         X = [view(mod.X, :, :, p) for p in 1:mod.P]
+        XtX = [X'[p] * X[p] for p in 1:mod.P]
         # lagged effect
-        kout = [kernel(tvec, μ[p], λ[p]) for p in 1:mod.P]
-        β = [kout[p][1] for p in 1:mod.P]
+        kernels = [kernel(tvec, μ[p], λ[p]) for p in 1:mod.P]
+        β = [kernels[p][1] for p in 1:mod.P]
         ϕ = [X[p] * β[p] for p in 1:mod.P]
         # errors
-        y_hat = sum(γ .* ϕ)
-        new_α += mean(y_hat .- y) / (mod.R * mod.C)
-        ε = mod.α + y_hat .- y
-        starting_loss += 0.5 * sum(ε .^ 2)
+        Φ = sum(γ .* ϕ)
+        cum_error += mean(Φ .- y)
+        ε = mod.α .+ Φ .- y
+        prev_loss += 0.5 * sum(ε .^ 2)
         # can use multithread here with small overhead
         @threads for p in 1:mod.P
             nbrs = spatial_neighbors(mod, p, r, c)
-        
+            ∂β∂μ, ∂β∂λ = kernels[p][2]  # size t each
+            ∂²β∂²μ, ∂²β∂²λ = kernels[p][3]  # size t each
+
             # mu update
             μ_nbrs_sum = sum(mod.μ[nbrs])
-            starting_loss += mod.η * (μ[p] - μ_nbrs_sum)^2
-            ∂β∂μ = kout[p][2][1]  # size t
-            ∂²β∂²μ = kout[p][3][1]  # size t
-            ∇ = dot(X[p] * ∂β∂μ, ε) + mod.η * (μ[p] - μ_nbrs_sum)
-            ∇² = dot(X[p] * ∂²β∂²μ, ε) + mod.η
+            prev_loss += 0.25 * mod.η * (μ[p] - μ_nbrs_sum)^2
+            ∇ = γ[p] * ε' * X[p] * ∂β∂μ +
+                mod.η * (μ[p] - μ_nbrs_sum)
+            ∇² = γ[p]^2 * ∂β∂μ' * XtX[p] * ∂β∂μ +
+                γ[p] * ε' * X[p] * ∂²β∂²μ +
+                mod.η * length(nbrs)
             mod.μ[p, r, c] -= ∇ / ∇²
 
             # lam update
             λ_nbrs_sum = sum(mod.λ[nbrs])
-            starting_loss += mod.η * (λ[p] - λ_nbrs_sum)^2
-            ∂β∂λ = kout[p][2][2]  # size t
-            ∂²β∂²λ = kout[p][3][2]  # size t
-            ∇ = dot(X[p] * ∂β∂λ, ε) + mod.η * (λ[p] - λ_nbrs_sum)
-            ∇² = dot(X[p] * ∂²β∂²λ, ε) + mod.η  # scalar
+            prev_loss += 0.25 * mod.η * (λ[p] - λ_nbrs_sum)^2
+            ∇ = γ[p] * ε' * X[p] * ∂β∂λ +
+                mod.η * (λ[p] - λ_nbrs_sum)
+            ∇² = γ[p]^2 * ∂β∂λ' * XtX[p] * ∂β∂λ +
+                γ[p] * ε' * X[p] * ∂²β∂²λ +
+                mod.η * length(nbrs)
             mod.λ[p, r, c] -= ∇ / ∇²
 
             # gamma updates (exact)
             γ_nbrs_sum = sum(mod.γ[nbrs])
-            starting_loss += mod.η * (γ[p] - γ_nbrs_sum)^2
-            starting_loss += mod.ν * γ[p]^2
-            γ_lp =  γ[p] * ϕ[p] .- ε
-            b =  dot(ϕ[p], γ_lp) + mod.η * γ_nbrs_sum
-            H = dot(ϕ[p], ϕ[p]) + mod.η * length(nbrs) + mod.ν
+            prev_loss += 0.25 * mod.η * (γ[p] - γ_nbrs_sum)^2
+            prev_loss += mod.ν * γ[p]^2
+            b =  γ[p] * ϕ[p] .- ε
+            H = ϕ[p]' * ϕ[p] + mod.η * length(nbrs) + mod.ν
             mod.γ[p, r, c] = b / H
         end
     end
-    mod.α = new_α
-    return starting_loss
+    # update alpha (this is a approx. delayed update for \alpha)
+    mod.α = cum_error / (mod.R * mod.C + mod.ν)
+    return prev_loss
 end
 
 
